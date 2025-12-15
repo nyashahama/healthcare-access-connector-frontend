d0ce6e5a50565e7558b030f2c27d339194218110
Nyasha Hama <nyashahama45@gmail.com>
2025-12-15 19:14:16 +0200

    added an auth image

--- src/layouts/auth/index.jsx
+++ src/layouts/auth/index.jsx
@@ -1,5 +1,5 @@
 import Footer from "components/footer/FooterAuthDefault";
-import authImg from "assets/img/auth/auth.png";
+import authImg from "assets/img/auth/auth_health.jpg";
 import { Link, Routes, Route, Navigate } from "react-router-dom";
 import { authRoutes } from "routes.js";
 import FixedPlugin from "components/fixedPlugin/FixedPlugin";
